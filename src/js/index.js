2619538c0de879cadf9d9c54f491d489a953dc1a
Martin Pilaszek <martinpilaszek@gmail.com>
2021-05-17 23:12:33 -0300

    Configuracion de webpack

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../styles/main.scss'
+
+const $title = document.getElementById('title')
+
+setTimeout(() => {
+    $title.textContent = 'javascript funca'
+}, 4000);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    "presets": ["@babel/preset-env"]
+                }
+            },
+            {
+                test: /\.(sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/[main].css'
+        })
+    ]
+}
